40e45cb23a74d8d23fc8b87b8cb978e69a7c108a
Sean C. Ryan <piefayth@gmail.com>
2014-10-28 21:18:25 -0500

    Expanded saved datatypes, introduced support for characters and frequencies.

--- GetErDone.lua
+++ GetErDone.lua
@@ -40,42 +40,132 @@ local options = {
 							GetErDone:AddQuest(v)
 						end
 					},
+					separator = {
+						order = 5,
+						type = "description",
+						name = "",
+					},
+					frequency = {
+						order = 6,
+						type = "select",
+						name = "Frequency",
+						desc = "How often should this item reset?",
+						style = "dropdown",
+						values = {["daily"] = "Daily", ["weekly"] = "Weekly", ["once"] = "Once"},
+						set = function(k,v)
+							GetErDone:ApplyOption(k,v)
+						end,
+						get = function()
+							return GetErDone:GetOption("frequency")
+						end
+					},
+					character = {
+						order = 7,
+						type = "select",
+						name = "Character",
+						desc = "Which character is this task for? Your character will not appear in this list until you've logged in with it.",
+						style = "dropdown",
+						values = function()
+							t = {["All"] = "All"}
+							for k, v in pairs(GetErDone:GetOption("characters")) do
+								print(k..v)
+								t[k] = v
+							end
+							return t
+						end,
+						set = function(k,v)
+							GetErDone:ApplyOption(k,v)
+						end,
+						get = function()
+							return GetErDone:GetOption("character")
+						end
+					}
 				},
 			},
 		},
 	}
 
+--ApplyOption is for option retention between sessions. If you click "Daily" then reloadui, it retains that selection--
+--The default behavior makes dropdowns always blank, which is annoying--
+function GetErDone:ApplyOption(k,v)
+	if (k[2] == "frequency") then
+		self.db.global.frequency = v
+	elseif (k[2] == "character") then
+		self.db.global.character = v
+	end
+end
+
+function GetErDone:GetOption(v)
+	return self.db.global[v]
+end
+
 function GetErDone:AddMonster(id)
-	table.insert(self.db.char.monsters, id)
-	return
+	--NEED TO CHECK IF MONSTER ID ALREADY EXISTS FIRST, THIS ASSUMES IT DOESNT
+	table.insert(self.db.global.monsters, {["monsterid"] = id, 
+										["frequency"] = self.db.global.frequency,
+										["characters"] = {self.db.global.character}})
 end
 
 function GetErDone:AddQuest(id)
-	table.insert(self.db.char.quests, id)
-	return
+	--NEED TO CHECK IF MONSTER ID ALREADY EXISTS FIRST, THIS ASSUMES IT DOESNT
+	table.insert(self.db.global.quests, {["questid"] = id, 
+										["frequency"] = self.db.global.frequency,
+										["characters"] = {self.db.global.character}})
 end
 
 function GetErDone:OnInitialize()
-	AceConfig:RegisterOptionsTable("GetErDone", options, {"ged", "geterdone"})
+	AceConfig:RegisterOptionsTable("GetErDone", options, {"ged", "geterdone"}) --TODO: Make these slash commands just open the menu
 	self.db = LibStub("AceDB-3.0"):New("GetErDoneDB")
 
-	---First Time Setup---
-	if self.db.char.monsters == nil then self.db.char.monsters = {} end
-	if self.db.char.quests == nil then self.db.char.quests = {} end
-	---
+
 	
 	self.optionsFrames = {}
 	self.optionsFrames.general = AceConfigDialog:AddToBlizOptions("GetErDone", nil, nil, "general")
+
+
+	--Event Registry--
+	self:RegisterEvent("PLAYER_ENTERING_WORLD", "OnLogin") --Note the syntax, second parameter is a function name as a string
+
 end
 
 function GetErDone:OnEnable()
-	for i, v in ipairs(self.db.char.monsters) do
-		print("Index: " .. i .. " Monster ID: " .. v)
+
+	---First Time Setup l---
+	if self.db.global.monsters == nil then self.db.global.monsters = {} end
+	if self.db.global.quests == nil then self.db.global.quests = {} end
+	if self.db.global.frequency == nil then self.db.global.frequency = "" end
+	if self.db.global.characters == nil then self.db.global.characters = {} end
+	name, server = UnitFullName("player")
+	if self.db.global.characters[name..server] == nil then 
+		self.db.global.characters[name..server] = name .. " - " .. server
 	end
-	for i, v in ipairs(self.db.char.quests) do
-		print("Index: " .. i .. " Quest ID: " .. v)
+	---
+
+	for i, v in ipairs(self.db.global.monsters) do
+		if v["monsterid"] ~= nil then
+			print("Index: " .. i .. " Monster ID: " .. v.monsterid)
+		end
 	end
+	for i, v in ipairs(self.db.global.quests) do
+		if v["questid"] ~= nil then
+			print("Index: " .. i .. " Quest ID: " .. v.questid)
+		end
+	end
+	for k, v in pairs(self.db.global.characters) do --Have to use pairs over ipairs for non numerical indices, afaik
+		print(k .. v)
+	end
+
+
 end
 
 function GetErDone:OnDisable()
 end
+
+---Event Handlers---
+
+---OnLogin defaults the "character" dropdown to the character you're currently logged in as.
+function GetErDone:OnLogin()
+	name, server = UnitFullName("player")
+	self.db.global.character = name..server
+end
+
